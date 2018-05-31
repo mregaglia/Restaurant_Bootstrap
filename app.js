89fb1896e80d9ce92696efc6d4f9f88e5a8585eb
Massimo Regaglia <m.regaglia@gmail.com>
2018-05-31 16:30:03 +0200

    31mai_16h30

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/Restaurant_Bootstrap/sw.js', { scope: '/Restaurant_Bootstrap/' }).then(function(reg) {
-    // registration worked
-    console.log('Registration succeeded. Scope is ' + reg.scope);
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('https://mregaglia.github.io/Restaurant_Bootstrap/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
   });
-};
+}
